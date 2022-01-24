7e70dfd0e9b48fc3bd7bf0c01a4a2078c6c44364
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-24 10:44:19 -0300

    chore: added project  dependencies

---
+++ container-module-federation/config/webpack.dev.js
@@ -0,0 +1,40 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webPackCommon = require('./webpack.common');
+
+const devConfig = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    // para webpack entender a extensão jsx
+    // se for usar tsx tambem precisa colocar aqui
+  },
+  devServer: {
+    static: {
+      directory: './public',
+    },
+    port: 4050,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      title: 'Marketing React',
+    }),
+  ],
+};
+
+module.exports = merge(webPackCommon, devConfig);
---
+++ container-module-federation/dist/src_bootstrap_jsx.js
@@ -0,0 +1,37 @@
+'use strict';
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self['webpackChunkcontainer'] = self['webpackChunkcontainer'] || []).push([
+  ['src_bootstrap_jsx'],
+  {
+    /***/ './src/app.jsx':
+      /*!*********************!*\
+  !*** ./src/app.jsx ***!
+  \*********************/
+      /***/ (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "App": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n\nvar App = function App() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "ola mundo");\n};\n\n//# sourceURL=webpack://container/./src/app.jsx?'
+        );
+
+        /***/
+      },
+
+    /***/ './src/bootstrap.jsx':
+      /*!***************************!*\
+  !*** ./src/bootstrap.jsx ***!
+  \***************************/
+      /***/ (__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");\n/* harmony import */ var _app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app */ "./src/app.jsx");\n\n\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.StrictMode, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_app__WEBPACK_IMPORTED_MODULE_2__.App, null)), document.querySelector("#root824627842"));\n\n//# sourceURL=webpack://container/./src/bootstrap.jsx?'
+        );
+
+        /***/
+      },
+  },
+]);
