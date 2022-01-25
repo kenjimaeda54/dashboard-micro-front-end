be6f166c9eac24a2c435b0d6696156e3646f6dbf
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-01-25 16:11:37 -0300

    fix: solved problem with path

--- container-module-federation/src/app.jsx
+++ container-module-federation/src/app.jsx
@@ -4,7 +4,7 @@ import MarketingReact from './components/marketing-react';
 export const App = () => {
   return (
     <div>
-      <h1>ola mundo</h1>
+      <h1>ola mundo!!!</h1>
       <hr />
       <MarketingReact />
     </div>
