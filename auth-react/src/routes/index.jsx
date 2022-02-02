91ba8a1aef41e908d2f8f77ef9a029700b49f57b
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-02-02 16:52:44 -0300

    feat: conection with aws

--- auth-react/src/routes/index.jsx
+++ auth-react/src/routes/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch, Router } from "react-router-dom";
 import { SignUp } from "../pages/sign-up";
 import { SignIn } from "../pages/sign";
-//
+
 export const RoutesApp = ({ onSingIn }) => {
   return (
     <Switch>
